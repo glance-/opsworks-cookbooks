1c8a59f2b4770145f8dd0d82a3c4b46c66a7ca0c
Anton Lundin <anton@dohi.se>
2014-06-12 14:33:01 +0200

    WIP: asset cache headers and gzipped delivery

--- passenger_apache2/recipes/rails.rb
+++ passenger_apache2/recipes/rails.rb
@@ -7,3 +7,7 @@ include_recipe "apache2"
 include_recipe "apache2::mod_deflate"
 include_recipe "passenger_apache2"
 include_recipe "passenger_apache2::mod_rails"
+
+if true
+  include_recipe "apache2::mod_expires"
+end
